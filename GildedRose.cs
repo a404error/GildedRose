a21b8928ca3e4f6959eeebc7a34faa15432c0cbc
a404error <bcjowett@gmail.com>
2022-03-22 12:35:24 -0400

    Initial commit

---
+++ GildedRose.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRose
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public static int _maxQuality = 50;
+        public static int _minQuality = 0;
+
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        /// <summary>
+        /// Determines if the original or updated code is to be run baseed on user input.
+        /// </summary>
+        /// <param name="choice">Numerical choice the user typed at the prompt.</param>
+        public void UpdateQuality(int choice)
+        {
+            switch (choice)
+                {
+                case 1:
+                    RunOriginalCode();
+                    break;
+                case 2:
+                    RunCleanedCode();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Updated code. It has been rewritten to be cleaner, easier to read, more maintainable, and easier to expand upon.
+        /// </summary>
+        public void RunCleanedCode()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                switch (Items[i].Name)
+                {
+                    case "Aged Brie":
+                        {
+                            // I was a little confused by the code given versus the conditions given for Aged Brie. In a real world setting, I would've just reached out to a BA, PM, or the client directly for clarification.
+                            // Since I didn't really have those resources, I coded it both ways and could simply delete the wrong one based on the response.
+
+                            // Increase quality of Aged Brie by 1. This is how I interpreted the requirements, but I could be wrong. I just don't see how it could ever increase if quality by 2 each day given the conditions.
+                            Items[i].Quality = Items[i].Quality < _maxQuality ? IncrementQuality(Items[i].Quality, 1) : Items[i].Quality;
+
+                            // Increase quality of Aged Brie by 2 once sell in date is less than zero. This is how the code works, but I think it's a bug. The original code (saved towards the bottom of this document) has Aged Brie decreasing in quality twice each day, but the requirements say otherwise.
+                            //Items[i].Quality = Items[i].Quality < _maxQuality ? IncrementQuality(Items[i].Quality, 2) : Items[i].Quality;
+
+                            break;
+                        }
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        {
+                            Items[i].Quality = (Items[i].SellIn <= 0) ? Items[i].Quality = 0 : // If the item's sell-in days is 0 or below - set it's quality to 0.
+                                               (Items[i].SellIn > 0 && Items[i].SellIn <= 5) ? IncrementQuality(Items[i].Quality, 3) : // If the item's sell-in days is between 1 and 5 - increase it's quality by 3.
+                                               (Items[i].SellIn > 5 && Items[i].SellIn <= 10) ? IncrementQuality(Items[i].Quality, 2) : // If the item's sell-in days is between 6 and 10 - increase it's quality by 2.
+                                               (Items[i].Quality < _maxQuality && Items[i].Quality != 0) ? IncrementQuality(Items[i].Quality, 1) : // If the item's quality is less than the max quality, and not equal to 0 - increase it's quality by 1.
+                                               Items[i].Quality; // No change to item's quality.
+
+                            Items[i].Quality = IsItemMaxQuality(Items[i].Quality);
+
+                            break;
+                        }
+                    case string item when Items[i].Name.Contains("Conjured"):
+                        {
+                            Items[i].Quality = (Items[i].Quality >= 1) ? DecrementQuality(Items[i].Quality, 2) : Items[i].Quality;
+
+                            break;
+                        }
+                    case "Sulfuras, Hand of Ragnaros":
+                        {
+                            // This item has a set quality. If detected, it'll bypass any logic that alters an item's quality.
+                            break;
+                        }
+                    default:
+                        {
+                            Items[i].Quality = (Items[i].Quality == 0) ? Items[i].Quality : // If the quality of an item is 0 - don't alter it.
+                                               (Items[i].SellIn <= 0) ? DecrementQuality(Items[i].Quality, 2) : // If the item's sell-in days is less than or equal to 0 - decrease it's quality by 2.
+                                               (Items[i].Quality > _minQuality) ? DecrementQuality(Items[i].Quality, 1) : // If the item's quality is higher than the minimum quality allowed - decrease it's quality by 1.         
+                                               Items[i].Quality; // No change to item's quality.
+
+                            break;
+                        }
+                }
+
+                ReduceSellInDays(Items[i], 1);
+            }
+        }
+
+        /// <summary>
+        /// Checks if the quality of an item is higher than the max allowed. If it is, set the item's quality to the max quality.
+        /// </summary>
+        /// <param name="itemQuality">Quality of the Item.</param>
+        /// <returns>The item's quality.</returns>
+        public int IsItemMaxQuality(int itemQuality)
+        {
+            return itemQuality = (itemQuality > _maxQuality) ? _maxQuality : itemQuality;
+        }
+
+        /// <summary>
+        /// Reduce the item's sell in days by one.
+        /// </summary>
+        /// <param name="item">Item object.</param>
+        public void ReduceSellInDays(Item item, int daysToReduce)
+        {
+            if (!item.Name.Equals("Sulfuras, Hand of Ragnaros"))
+            {
+                item.SellIn = item.SellIn - daysToReduce;
+            }
+        }
+
+        /// <summary>
+        /// Increase an item's quality based on the supplied parameter.
+        /// </summary>
+        /// <param name="itemQuality">Item's current quality.</param>
+        /// <param name="increaseQualityBy">Number to increase the item's quality by.</param>
+        /// <returns>Item's updated quality.</returns>
+        public int IncrementQuality(int itemQuality, int increaseQualityBy)
+        {
+            return itemQuality = itemQuality + increaseQualityBy;
+        }
+
+        /// <summary>
+        /// Decrease an item's quality based on the supplied parameter.
+        /// </summary>
+        /// <param name="itemQuality">Item's current quality.</param>
+        /// <param name="decreaseQualityBy">Number to decrease the item's quality by.</param>
+        /// <returns>Item's updated quality.</returns>
+        public int DecrementQuality(int itemQuality, int decreaseQualityBy)
+        {
+            return itemQuality = itemQuality - decreaseQualityBy;
+        }
+
+        /// <summary>
+        /// Original messy code. It has been updated with the new Conjured item.
+        /// </summary>
+        public void RunOriginalCode()
+        {
+            for (var i = 0; i < Items.Count; i++)
+            {
+                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros" && Items[i].Name != "Conjured Mana Cake")
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
+                        else if (Items[i].Name == "Conjured Mana Cake") // Code added to handle the new Conjured item.
+                        {
+                            Items[i].Quality = Items[i].Quality - 2;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].SellIn < 11)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+
+                            if (Items[i].SellIn < 6)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                }
+
+                if (Items[i].SellIn < 0)
+                {
+                    if (Items[i].Name != "Aged Brie")
+                    {
+                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].Quality > 0)
+                            {
+                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                {
+                                    Items[i].Quality = Items[i].Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleTables;
+
+namespace GildedRose
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+
+            var app = new GildedRose(Items);
+
+            try
+            {
+                Console.WriteLine("Press 1 to run the original code or 2 to run the updated code.");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice != 1 && choice != 2)
+                {
+                    throw new ArgumentException();
+                }
+
+                for (var i = 0; i < 31; i++)
+                {
+                    Console.WriteLine("-- Day " + i);
+
+                    // Create a new ConsoleTables object to better display the stores items. Improves readability of table-like data on a console app.
+                    var table = new ConsoleTable("Name", "Sell In", "Quality");
+
+                    for (var j = 0; j < Items.Count; j++)
+                    {
+                        table.AddRow(Items[j].Name, Items[j].SellIn, Items[j].Quality);
+                    }
+
+                    table.Options.EnableCount = false;
+                    table.Write();
+                    Console.WriteLine();
+
+                    app.UpdateQuality(choice);
+                }
+
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Allison's pet dragonhawk didn't like that and shoots a fireball as a warning shot.");
+                Console.WriteLine();
+                Items.Clear();
+                Main(new string[0]);
+            }
+        }
+    }
+}
